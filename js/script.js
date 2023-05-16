ebf88d0ebb9d289870d5ff0f46f7805ac6f1e1eb
william <124389603+william-simard@users.noreply.github.com>
2023-05-16 18:37:43 +0000

    work

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: william 
+// Created on: may 2023
 // This file contains the JS functions for index.html
+function calculate() {
+  // delcare variables
+  const aBase = parseInt(document.getElementById("a-base").value)
+  const bBase = parseInt(document.getElementById("b-base").value)
+  const height = parseInt(document.getElementById("height").value)
+
+  // calculate
+  const area = (aBase + bBase) / 2 * height
+
+  // output
+  document.getElementById("area").innerHTML = "The area is: " + area + " cm²"
+}
